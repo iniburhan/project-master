38ef4db4af6b8b1465100fba6352e2688d3bcc18
Magang MIS <magang.mis@operational.patria.co.id>
2023-10-25 13:12:05 +0700

    edit view register & forgot password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -63,3 +63,24 @@
     </div>
 </div>
 @endsection
+              <p class="mb-4">Enter your email and we'll send you instructions to reset your password</p>
+              <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    autofocus
+                  />
+                </div>
+                <button class="btn btn-primary d-grid w-100">Send Reset Link</button>
+              </form>
+              <div class="text-center">
+                <a href="auth-login-basic.html" class="d-flex align-items-center justify-content-center">
+                  <i class="bx bx-chevron-left scaleX-n1-rtl bx-sm"></i>
+                  Back to login
+                </a>
+              </div>
